28f2dc4862927b81ff0892db7ba2d00e8f12ad76
Jakub Kania <kubak.15@gmail.com>
2018-07-04 16:32:48 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Tabs from './component/tabs'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Tabs>
+
+        </Tabs>
+      </div>
+    );
+  }
+}
+
+export default App;
