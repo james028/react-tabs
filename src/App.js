d02cea714a68330df64725f973c11d96fef45eb9
Jakub Kania <kubak.15@gmail.com>
2018-07-04 16:52:20 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Tabs from './component/tabs'
+import Tabs from './components/tabs'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Tabs>
-
+          <div label="Gator">
+            See ya later, <em>Alligator</em>!
+          </div>
+          <div label="Croc">
+            After 'while, <em>Crocodile</em>!
+          </div>
+          <div label="Sarcosuchus">
+            Nothing to see here, this tab is <em>extinct</em>!
+          </div>
         </Tabs>
       </div>
     );
